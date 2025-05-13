1855d96b795d962ded7a1aa904a3553a84437548
sashkotenk <oleksandr_bondarchuk@knu.ua>
2025-05-13 21:02:51 +0300

    додано метод малювання для відображення привида

--- ghost.py
+++ ghost.py
@@ -10,3 +10,6 @@ class Ghost(Character):
         self.path_update_timer = 0
         self.image = pygame.image.load("assets/ghost.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (30, 30))
+
+    def draw(self, screen, color=None):
+        screen.blit(self.image, (self.position.x - 15, self.position.y - 15))
