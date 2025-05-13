a8ecde9c8a6426c8e088b6cc23d47785dec5283c
sashkotenk <oleksandr_bondarchuk@knu.ua>
2025-05-13 20:19:00 +0300

    ініціалізовано привида з налаштуванням положення, швидкості та зображення

---
+++ ghost.py
@@ -0,0 +1,12 @@
+import pygame
+import heapq
+from character import Character
+
+
+class Ghost(Character):
+    def __init__(self, x, y, speed=2):
+        super().__init__(x, y, speed)
+        self.path = []
+        self.path_update_timer = 0
+        self.image = pygame.image.load("assets/ghost.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 30))
